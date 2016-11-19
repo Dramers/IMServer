7c09677c0d97df55051641e6c8f53410fa375f45
yyllove123 <yyllove123@hotmail.com>
2016-11-19 18:17:29 +0800

    群消息 ok？

--- msgServer/module/msgService.js
+++ msgServer/module/msgService.js
@@ -1,11 +1,25 @@
 function MsgService(client) {
 	var db = require('./msgdb');
-		var dbManager = new db();
+	var dbManager = new db();
+
+	var groupDB = require('./groupDB');
+	var groupDBManager = new groupDB();
+
+	function queryGroup(groupId, callback) {
+		groupDBManager.query(groupId, callback);
+	}
+ 
+	function sendMessage(toUserId, message) {
+		var toClient = clientInfos.get(toUserId);
+		console.log(' sendMessage: ' + toClient + ' toUserId: ' + toUserId + ' Content: ' + message.contentStr);
+		if (toClient) { 
+			message["sendDate"] = data.sendDate;
+			toClient.emit('message', message);
+		};
+	}
 
 	client.on('message', function (data) {
-		var fromUserId = data.fromUserId;
-		var toUserId = data.toUserId;
-	
+
 		// save Msg
 		dbManager.addMsg(data, function (err, doc) {
 			if (err) { return client.emit('receiveError', err)};
@@ -15,14 +29,32 @@ function MsgService(client) {
 				"msgId" : doc.msgId,
 				'status' : 2
 			});
+			
+			var sessionId = data.sessionId;
+			if (sessionId) {
+				// 群组消息
+				// 获取群组
+				queryGroup(sessionId, function (err, doc) {
 
+					// 未找到群组信息
+					if (err) { return };
 
-			var toClient = clientInfos.get(toUserId);
-			console.log(' : ' + toClient + ' toUserId: ' + toUserId);
-			if (toClient) { 
-				doc["sendDate"] = data.sendDate
-				toClient.emit('message', doc);
-			};
+					var fromUserId = data.fromUserId;
+					var memberIds = doc.memberIds;
+					for (var i = memberIds.length - 1; i >= 0; i--) {
+						var userId = memberIds[i];
+						
+
+						if (userId != fromUserId) {
+							sendMessage(userId, doc);
+						};
+					};
+				});
+			}
+			else {
+				// 个人消息
+				sendMessage(data.toUserId, doc);
+			}
 		});
 	});
 
