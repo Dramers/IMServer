868181c20bb47cd39d9bd24a60e4f9eb08234505
yyllove123 <yyllove123@hotmail.com>
2016-12-03 09:32:19 +0800

    删除 httpLoginServer

--- httpLoginServer/model/userModel.js
+++
@@ -1,9 +0,0 @@
-var Schema = require('mongoose').Schema;
-var mySchema = Schema({
-	userId : Number,
-	name : String,
-	username : String,
-	password : String
-});
-
-module.exports = db.model('UserModel', mySchema);
